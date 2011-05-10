87e05dbbec6b904f51a6a61d05c7aab1ab7b1242
Carl Boettiger <cboettig@gmail.com>
2011-05-09 19:02:29 -0700

    updated examples script to illustrate a few different queries

--- demos/search.R
+++ demos/search.R
@@ -7,14 +7,18 @@ require(treebase)
 
 price_trees <- search_treebase("Price", by="author")
 
-## Need to say what the multiples are of.  must be in the same section
+## Note that by must identify each entry type if a Boolean is given
 HR_trees <- search_treebase("Ronquist or Hulesenbeck", by=c("author", "author"))
-
 Huelsenbeck <- search_treebase("Huelsenbeck", by="author")
 
 # notice the quotes
-whales <- search_treebase('"Cetecea"', by="taxon")
-
 dolphins <- search_treebase('"Delphinus"', by="taxon")
-
 humans <- search_treebase('"Homo sapiens"', by="taxon", exact_match=TRUE)
+
+five <- search_treebase(5, by="ntax")
+
+studies <- search_treebase("2377", by="id.study")
+tree <- search_treebase("2377", by="id.tree")
+
+#whales <- search_treebase('"Cetecea"', by="taxon")
+
