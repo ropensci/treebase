b2275b35f86318122d13314d7832a93b84f084d9
Carl Boettiger <cboettig@gmail.com>
2012-01-04 15:01:22 -0800

    data cache and documentation

---
+++ R/data.R
@@ -0,0 +1,34 @@
+#' metadata.rda
+#' 
+#' All metadata from treebase publications, Created 2011-12-12
+#' @name all
+#' @docType data
+#' @keywords data
+#' @details recreate with \code{ 
+#' all <- search_metadata("", by="all") 
+#' }
+NULL
+
+#' branchlengths.rda
+#' 
+#' All treebase trees with branch length data, created 2011-12-12
+#' @name all_trees
+#' @docType data
+#' @keywords data
+#' @details recreate with code: \code{
+#' all_trees <- search_treebase("Consensus", "type.tree", branch_lengths=TRUE)
+#' }
+
+NULL
+
+#' treebase.rda
+#'
+#' Created 2011-12-12
+#' @name all_treebase
+#' @docType data
+#' @keywords data
+#' @details recreate with: \code{
+#' all_treebase <- search_treebase("Consensus", "type.tree")
+#' }
+#'
+NULL
