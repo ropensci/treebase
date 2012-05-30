bbe87438904a2f806431f15295557b53b38d90e3
Carl Boettiger <cboettig@gmail.com>
2012-05-29 18:39:30 -0700

    github copy

--- inst/tests/metadata.R
+++ inst/tests/metadata.R
@@ -1,5 +1,5 @@
 context("Metadata")
 
 test_that("metadata works as expected", {
-          expect_that(is(metadata(), "data.frame", is_true())
-}
+          expect_that(is(metadata(), "data.frame"), is_true())
+})
